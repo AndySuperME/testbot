dba1d16aee69f355be56348fa55a5c9d3ea2237a
AndySuperME <ayr56tdr59asa@gmail.com>
2018-01-24 23:00:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,49 @@ const weatherOpt = {
 }
 
 
+var myLineCarouselTemplate={
+    type: 'template',
+	altText: 'this is a carousel template',
+	template: {
+		type: 'carousel',
+		columns: [{
+			thumbnailImageUrl: 'https://example.com/bot/images/item1.jpg',
+			title: 'this is menu',
+			text: '按下選單可以查看目前PM2.5！\n輸入？即可再次出現選單\n點選之後請等待幾秒鐘的運算時間',
+			actions: [{
+                type: 'postback',
+                label: '臺南市',
+                data: '1:臺南,臺南',
+            }, {
+                type: 'postback',
+                label: '南投縣日月潭',
+                data: '1:南投,日月潭',
+            }, {
+                type: 'postback',
+                label: '高雄市左營',
+                data: '1:左營,高雄',
+            }]
+		}, {
+			thumbnailImageUrl: 'https://example.com/bot/images/item2.jpg',
+			title: '設定通知地點/溫度',
+			text: 'description',
+			actions: [{
+				type: 'postback',
+				label: '查看地點',
+				data: '2:查看地點'
+			}, {
+				type: 'postback',
+				label: '設置地點',
+				data: '3:設置地點'
+			}, {
+				type: 'uri',
+				label: '設置通知溫度',
+				uri: '4:設置溫度'
+			}]
+		}]
+	}
+}
+/*
 var myLineTemplate={
     type: 'template',
     altText: 'this is a confirm template',
@@ -39,7 +82,7 @@ var myLineTemplate={
         }]
     }
 };
-
+*/
 var bot = linebot({
     channelId: process.env.CHANNEL_ID,
 	channelSecret: process.env.CHANNEL_SECRET,
@@ -98,23 +141,43 @@ bot.on('message', function(event) {
                 queryDatabase(clientID, tmpdata);
                 break;
             case '？':
-                event.reply(myLineTemplate);
+                event.reply(myLineCarouselTemplate);
                 break;
 
             case '?':
-                event.reply(myLineTemplate);
+                event.reply(myLineCarouselTemplate);
                 break;
         }
     }
 });
 
 bot.on('postback', function (event) {
-    var city = event.postback.data.substring(0, event.postback.data.indexOf(','));
-    var locationName = event.postback.data.substring(event.postback.data.indexOf(',')+1, event.postback.data.length);
-    clientID = event.source.userId;
-    event.reply(sendPMMsg(clientID, city, locationName));
+    var firsttmp = event.postback.data.substring(0, event.postback.data.indexOf(':'));
+    switch (firsttmp) {
+        case '1':  
+            var city = event.postback.data.substring(event.postback.data.indexOf(':')+1, event.postback.data.indexOf(','));
+            var locationName = event.postback.data.substring(event.postback.data.indexOf(',')+1, event.postback.data.length);
+            clientID = event.source.userId;
+            event.reply(sendPMMsg(clientID, city, locationName));
+            console.debug( clientID + " 按下:" + city + '/' + locationName);
+            break;
+        case '2':
+            event.reply('南沙島 /東沙島 /馬祖 /金門 /東吉島 /澎湖 /蘭嶼 /大武 /臺東 /成功 /花蓮 /恆春 /高雄 /臺南 /嘉義 /阿里山 /玉山 /日月潭 /梧棲 /臺中 /新竹 /新屋 /板橋 /臺北 /陽明山 /鞍部 /蘇澳 /宜蘭 /基隆 /彭佳嶼');
+            break;
+        case '3':
+            event.reply('請直接回覆地點 \nP.S. 回覆前請先查看有哪些地點！！');
+            bot.on('message', function (event1){
+                var userSendMsg = event1.message.text;
+                queryDatabase(event.source.userId, userSendMsg);
+                event1.reply('已設定地點於：' + userSendMsg);
+            })
+            break;
+        case '4':
+            
+            break;
+    }
 
-    console.debug( clientID + " 按下:" + city + '/' + locationName);
+   
 }) 
 
 const linebotParser = bot.parser(),

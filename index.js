144d9a071853700f9d845377c92718f1b1fc9052
AndySuperME <ayr56tdr59asa@gmail.com>
2018-01-26 16:09:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var linebot = require('linebot'),
-    express = require('express'),
-    dateFormat = require('dateformat');
+    express = require('express');
 
 var SITE_NAME = '臺南', SITE_NAME2 = '', SITE_NAME3 = '';
 
@@ -17,49 +16,6 @@ const weatherOpt = {
     json: true
 }
 
-
-var myLineCarouselTemplate={
-    type: 'template',
-	altText: 'this is a carousel template',
-	template: {
-		type: 'carousel',
-		columns: [{
-			thumbnailImageUrl: 'https://dynamicmedia.zuza.com/zz/m/original_/3/d/3d553796-0322-4e21-b100-20ca39ca3494/hot_sun_cooling___Gallery.jpg',
-			title: '天氣狀況',
-			text: '按下選單可以查看目前PM2.5！\n輸入？即可再次出現選單\n點選之後請等待幾秒鐘的運算時間',
-			actions: [{
-                type: 'postback',
-                label: '臺南市',
-                data: '1:臺南,臺南',
-            }, {
-                type: 'postback',
-                label: '南投縣日月潭',
-                data: '1:南投,日月潭',
-            }, {
-                type: 'postback',
-                label: '高雄市左營',
-                data: '1:左營,高雄',
-            }]
-		}, {
-			thumbnailImageUrl: 'https://i.imgur.com/xQF5dZT.jpg',
-			title: '設定通知地點/溫度',
-			text: 'description',
-			actions: [{
-				type: 'postback',
-				label: '查看地點',
-				data: '2:查看地點'
-			}, {
-				type: 'postback',
-				label: '設置地點',
-				data: '3:設置地點'
-			}, {
-                type: 'postback',
-                label: '設置通知時間',
-                data: '4:設置時間'
-            }]
-		}]
-	}
-}
 
 var bot = linebot({
    
@@ -116,8 +72,8 @@ bot.on('message', function(event) {
         var tmpdata = msg.substring(msg.indexOf(':')+1, msg.length-1);
         console.log("======="+tmpdata+"=====");
         switch (tmp) {
-            case 'SetLocation': 
-                queryDatabase(clientID, tmpdata);
+            case 'info': 
+                event.reply('Author： Andy Tai \nGmail： ayr56tdr59asa\nPhone：');
                 break;
             case '？':
                 event.reply(movieTemplate);
@@ -125,7 +81,7 @@ bot.on('message', function(event) {
             case '?':
                 event.reply(myLineCarouselTemplate);
                 break;
-            case '地點':
+            case 'L':
                 queryDatabase(clientID, tmpdata);
                 event.source.profile().then(function (profile) {
                     event.reply(profile.displayName + ' 您已設定地點於：' + tmpdata);
@@ -133,23 +89,24 @@ bot.on('message', function(event) {
                     // error
                 });
                 break;
-            case '時間':
-                /*
+            /*    
+            case 'T':
                 event.source.profile().then(function (profile) {
+                    queryUpdateTimeDataBase(event.source.userId, tmpdata);
                     event.reply(profile.displayName + ' 您已設定通知時間於：' + tmpdata);
                 }).catch(function (error) {
                     // error
                 });
-                */
                 break;
-            case 'getDataBaseData':
-                getDataBaseData(event);
-                break;
+            */
             case 'M':
                 queryMovieDatabase(tmpdata);
                 movieData.then(function(object){
                     bot.push(event.source.userId ,object.name + "\n" + object.url + "\n" + object.descri + "\n" + object.infor);  
                 })       
+                break;
+            case 'getDataBaseData':
+                getDataBaseData(event);
                 break;
         }
     }
@@ -173,10 +130,16 @@ bot.on('postback', function (event) {
             event.reply('南沙島 /東沙島 /馬祖 /金門 /東吉島 /澎湖 /蘭嶼 /大武 /臺東 /成功 /花蓮 /恆春 /高雄 /臺南 /嘉義 /阿里山 /玉山 /日月潭 /梧棲 /臺中 /新竹 /新屋 /板橋 /臺北 /陽明山 /鞍部 /蘇澳 /宜蘭 /基隆 /彭佳嶼');
             break;
         case '3':
-            event.reply('請直接回覆地點 \n EX: 地點:臺南(請勿使用全形：) \n P.S. 回覆前請先查看有哪些地點！！');   
+            event.reply('請直接回覆地點 \nEX: L:臺南(請勿使用全形：) \nP.S. 回覆前請先查看有哪些地點！！');   
             break;
         case '4':
-            event.reply('請直接回覆通知時間 \n EX: 時間:08 (請勿使用全形： 請以小時填寫)')
+            //event.reply('請直接回覆通知時間 \nEX: T:08 (請勿使用全形： 請以小時填寫) \nP.S. 通知時間為 06~23');
+            event.source.profile().then(function (profile) {
+                queryUpdateTimeDataBase(event.source.userId, event.postback.params.time);
+                event.reply(profile.displayName + ' 您已設定通知時間於：' + event.postback.params.time);
+            }).catch(function (error) {
+                // error
+            });
             break;
         case '5':
             queryMovieTimeDatabase("02");
@@ -187,7 +150,7 @@ bot.on('postback', function (event) {
             })
             break;
         case '6':
-            event.reply('輸入電影關鍵字來查詢 \n EX：M:阿凡達 （輸入阿即可）\nP.S. 記得使用半形:');
+            event.reply('輸入電影關鍵字來查詢 \nEX：  M:阿凡達 （輸入阿即可）\nP.S. 記得使用半形:');
             break;
     }
 
@@ -210,25 +173,8 @@ app.listen(process.env.PORT || 8080, function () {
 
 
 setInterval(function(){
-    var now = new Date();
-    dateFormat.masks.hammerTime = 'HH';
-    var h = dateFormat(now, "hammerTime");
-    //console.log(h);
-    /*
-    if (h == '07' || h == '08'|| h == '09'|| h == '10'|| h == '11'|| h == '12'|| h == '13'|| h == '14'|| h == '15'|| h == '16'|| h == '17'|| h == '18'|| h == '19'|| h == '20'|| h == '21'|| h == '22'|| h == '23'){
-        //pingWeb();
-        rp2(weatherOpt)
-        .then(function (repos) {
-            var weatherData3 = readWEATHER3(repos);
-            var temperature = weatherData3.Temperature;
-            var temperature = temperature.substring(0, temperature.indexOf('('));
-            if (temperature > 25)
-            bot.push('U4575fdaaae002fb2b9b67be60354de7c', temperature);
-        })
-    }
-    */
     alertWeather();
-}, 600000)
+}, 3000)
 
 function sendPMMsg(ID, city, locationName) {
     SITE_NAME = city;
@@ -354,9 +300,9 @@ function getDataBaseData(event) {
                     console.log(res.rowCount);
                     rows.map(row => {
                         //console.log(`Read: ${JSON.stringify(row)}`);
-                        var data = row.name;
-                        console.log(data);
-                        resolve(data);
+                        //var data = row.name;
+                        //console.log(data);
+                        resolve(row);
                     });
                 })
                 .catch(err => {
@@ -364,7 +310,7 @@ function getDataBaseData(event) {
                 });
     }).then(function(value){
             event.source.profile().then(function(profile){
-            event.reply(profile.displayName + " 您已設定區域於 " + value);
+            event.reply(profile.displayName + " 您已設定區域於 " + value.name + "\n時間：" + value.alerttime);
         })
     })
 }
@@ -373,21 +319,35 @@ function getDataBaseData(event) {
 
 /*---------------------------------------------------------*/
 function alertWeather() {
+    var now = new Date();
     const query = 'SELECT * FROM inventory;';
-    sqlArray = [];
     client.query(query)
         .then(res => {
             const rows = res.rows;
             rows.map(row => {
-                //SITE_NAME3 = row.name;
-                //var getID = row.id;
                 rp2(weatherOpt)
                     .then(function (repos) {
                         var weatherData3 = readWEATHER3(repos, row.name);
                         var temperature = weatherData3.Temperature;
                         var temperature = temperature.substring(0, temperature.indexOf('('));
-                        bot.push(row.id, temperature);    
-                       })        
+                        var humidity = weatherData3.Moisture;
+                        var weather = weatherData3.Weather;
+                        var date = weatherData3.DataCreationDate;
+                        var t = now.getHours()+8 + ':' + now.getMinutes();
+                        var tmp = t.substring(0, t.indexOf(':'));
+                        if (tmp.length == 1) {
+                            t = '0' + t;
+                        }
+                        
+                        if (t == row.alerttime){
+                            bot.push(row.id, "目前溫度：" + temperature + "\n" +
+                                             "目前濕度：" + humidity + "\n" +
+                                             "目前狀態：" + weather + "\n" +
+                                             "更新時間：" + date
+                            );
+                        }
+                        
+                    })        
             });
         })
         .catch(err => {
@@ -430,6 +390,50 @@ function getCurrentWeatherByGeoCoordinates(event) {
         }).then(function(){
             event.reply("目前溫度：" + temp + "\n目前濕度：" + humi + "\n目前狀態：" + state);
         })
+}
+
+var myLineCarouselTemplate={
+    type: 'template',
+	altText: 'this is a carousel template',
+	template: {
+		type: 'carousel',
+		columns: [{
+			thumbnailImageUrl: 'https://dynamicmedia.zuza.com/zz/m/original_/3/d/3d553796-0322-4e21-b100-20ca39ca3494/hot_sun_cooling___Gallery.jpg',
+			title: '天氣狀況',
+			text: '按下選單可以查看目前PM2.5！\n輸入？即可再次出現選單\n點選之後請等待幾秒鐘的運算時間',
+			actions: [{
+                type: 'postback',
+                label: '臺南市',
+                data: '1:臺南,臺南',
+            }, {
+                type: 'postback',
+                label: '南投縣日月潭',
+                data: '1:南投,日月潭',
+            }, {
+                type: 'postback',
+                label: '高雄市左營',
+                data: '1:左營,高雄',
+            }]
+		}, {
+			thumbnailImageUrl: 'https://i.imgur.com/xQF5dZT.jpg',
+			title: '設定通知地點/時間',
+			text: '(1)請按下查看地點查看有哪些地方可設定\n(2)在按下設置地點來設定\n(3)設定通知時間',
+			actions: [{
+				type: 'postback',
+				label: '查看地點',
+				data: '2:查看地點'
+			}, {
+				type: 'postback',
+				label: '設置地點',
+				data: '3:設置地點'
+			}, {
+                type: 'datetimepicker',
+                label: '設置通知時間',
+                data: '4:設置時間',
+                mode: 'time'
+            }]
+		}]
+	}
 }
 /*---------------------------------------------------------*/
 var movieData;
@@ -475,12 +479,29 @@ function queryMovieTimeDatabase(movieTime){
     })
 }
 
+function queryUpdateTimeDataBase(insertID, setTime) {
+    const query = "UPDATE inventory SET alerttime=" + "'" + setTime + "'" +" WHERE id = " + "'" + insertID + "'";
+   // console.log(query);
+    client
+        .query(query)
+        .then(result => {
+            console.log('Update completed');
+            console.log(`Rows affected: ${result.rowCount}`);
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+}
+
 var movieTemplate={
     type: 'template',
     altText: 'this is a confirm template',
     template: {
+        thumbnailImageUrl: 'https://ryanjayreviews.com/wp-content/uploads/2013/10/RJMC-4-Web-2.png',
+		title: '電影查詢',
         type: 'buttons',
-        text: '請先按下',
+        text: '(1)可以選擇近期電影來查看近期所有電影\n(2)可以輸入電影名稱關鍵字來查詢',
         actions: [{
             type: 'postback',
             label: '近期電影',

5717198f5650137ea9f2e3c1bbb297b5015b4a7e
AndySuperME <ayr56tdr59asa@gmail.com>
2018-01-27 18:20:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -341,31 +341,30 @@ function alertWeather() {
         .then(res => {
             const rows = res.rows;
             rows.map(row => {
-                rp2(weatherOpt)
+                if (t == row.alerttime && row.already != 'true'){
+                    rp2(weatherOpt)
                     .then(function (repos) {
-                    
-                        if (t == row.alerttime && row.already != 'true'){
-                            var weatherData3 = readWEATHER3(repos, row.name);
-                            var temperature = weatherData3.Temperature;
-                            var temperature = temperature.substring(0, temperature.indexOf('('));
-                            var humidity = weatherData3.Moisture;
-                            var weather = weatherData3.Weather;
-                            var date = weatherData3.DataCreationDate;
-                            var t = now.getHours()+2 + ':' + now.getMinutes();
-                            var tmp = t.substring(0, t.indexOf(':'));
-                            if (tmp.length == 1) {
-                                t = '0' + t;
-                            }
-                            updataAlreadyDatabase(row.id, "true");
-                            bot.push(row.id, "目前溫度：" + temperature + "\n" +
-                                             "目前濕度：" + humidity + "\n" +
-                                             "目前狀態：" + weather + "\n" +
-                                             "更新時間：" + date
-                            );
-                            setTimeout(function(){updataAlreadyDatabase(row.id, "false");}, 60000);
+                        var weatherData3 = readWEATHER3(repos, row.name);
+                        var temperature = weatherData3.Temperature;
+                        var temperature = temperature.substring(0, temperature.indexOf('('));
+                        var humidity = weatherData3.Moisture;
+                        var weather = weatherData3.Weather;
+                        var date = weatherData3.DataCreationDate;
+                        var t = now.getHours()+2 + ':' + now.getMinutes();
+                        var tmp = t.substring(0, t.indexOf(':'));
+                        if (tmp.length == 1) {
+                            t = '0' + t;
                         }
                         
-                    })        
+                    }) 
+                    updataAlreadyDatabase(row.id, "true");
+                    bot.push(row.id, "目前溫度：" + temperature + "\n" +
+                                     "目前濕度：" + humidity + "\n" +
+                                     "目前狀態：" + weather + "\n" +
+                                     "更新時間：" + date
+                    );
+                    setTimeout(function(){updataAlreadyDatabase(row.id, "false");}, 60000);
+                }       
             });
         })
         .catch(err => {
